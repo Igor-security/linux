a942147bbec953c61952b85d36406e2fcf7a9278
Igor Stoppa <igor.stoppa@huawei.com>
2018-03-27 18:37:39 +0300

    Protectable Memory
    The MMU available in many systems running Linux can often provide R/O
    protection to the memory pages it handles.
    
    However, the MMU-based protection works efficiently only when said pages
    contain exclusively data that will not need further modifications.
    
    Statically allocated variables can be segregated into a dedicated
    section (that's how __ro_after_init works), but this does not sit very
    well with dynamically allocated ones.
    
    Dynamic allocation does not provide, currently, any means for grouping
    variables in memory pages that would contain exclusively data suitable
    for conversion to read only access mode.
    
    The allocator here provided (pmalloc - protectable memory allocator)
    introduces the concept of pools of protectable memory.
    
    A module can instantiate a pool, and then refer any allocation request to
    the pool handler it has received.
    
    A pool is organized ias list of areas of virtually contiguous memory.
    Whenever the protection functionality is invoked on a pool, all the
    areas it contains that are not yet read-only are write-protected.
    
    The process of growing and protecting the pool can be iterated at will.
    Each iteration will prevent further allocation from the memory area
    currently active, turn it into read-only mode and then proceed to
    secure whatever other area might still be unprotected.
    
    Write-protcting some part of a pool before completing all the
    allocations can be wasteful, however it will guarrantee the minimum
    window of vulnerability, sice the data can be allocated, initialized
    and protected in a single sweep.
    
    There are pros and cons, depending on the allocation patterns, the size
    of the areas being allocated, the time intervals between initialization
    and protection.
    
    Dstroying a pool is the only way to claim back the associated memory.
    It is up to its user to avoid any further references to the memory that
    was allocated, once the destruction is invoked.
    
    An example where it is desirable to destroy a pool and claim back its
    memory is when unloading a kernel module.
    
    A module can have as many pools as needed.
    
    Since pmalloc memory is obtained from vmalloc, an attacker that has
    gained access to the physical mapping, still has to identify where the
    target of the attack (in virtually contiguous mapping) is located.
    
    Compared to plain vmalloc, pmalloc does not generate as much TLB
    trashing, since it can host multiple allocations in the same page,
    where present.
    
    Signed-off-by: Igor Stoppa <igor.stoppa@huawei.com>

---
+++ include/linux/pmalloc.h
@@ -0,0 +1,163 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * pmalloc.h: Header for Protectable Memory Allocator
+ *
+ * (C) Copyright 2017-18 Huawei Technologies Co. Ltd.
+ * Author: Igor Stoppa <igor.stoppa@huawei.com>
+ */
+
+#ifndef _LINUX_PMALLOC_H
+#define _LINUX_PMALLOC_H
+
+
+#include <linux/string.h>
+#include <linux/slab.h>
+
+/*
+ * Library for dynamic allocation of pools of protectable memory.
+ * A pool is a single linked list of vmap_area structures.
+ * Whenever a pool is protected, all the areas it contain at that point
+ * are write protected.
+ * More areas can be added and protected, in the same way.
+ * Memory in a pool cannot be individually unprotected, but the pool can
+ * be destroyed.
+ * Upon destruction of a certain pool, all the related memory is released,
+ * including its metadata.
+ *
+ * Pmalloc memory is intended to complement __read_only_after_init.
+ * It can be used, for example, where there is a write-once variable, for
+ * which it is not possible to know the initialization value before init
+ * is completed (which is what __read_only_after_init requires).
+ *
+ * It can be useful also where the amount of data to protect is not known
+ * at compile time and the memory can only be allocated dynamically.
+ *
+ * Finally, it can be useful also when it is desirable to control
+ * dynamically (for example throguh the command line) if something ought
+ * to be protected or not, without having to rebuild the kernel (like in
+ * the build used for a linux distro).
+ */
+
+
+#define PMALLOC_REFILL_DEFAULT (0)
+#define PMALLOC_ALIGN_DEFAULT ARCH_KMALLOC_MINALIGN
+
+struct pmalloc_pool *pmalloc_create_custom_pool(size_t refill,
+						unsigned short align_order);
+
+/**
+ * pmalloc_create_pool() - create a protectable memory pool
+ *
+ * Shorthand for pmalloc_create_custom_pool() with default argument:
+ * * refill is set to PMALLOC_REFILL_DEFAULT
+ * * align_order is set to PMALLOC_ALIGN_DEFAULT
+ *
+ * Return:
+ * * pointer to the new pool	- success
+ * * NULL			- error
+ */
+static inline struct pmalloc_pool *pmalloc_create_pool(void)
+{
+	return pmalloc_create_custom_pool(PMALLOC_REFILL_DEFAULT,
+					  PMALLOC_ALIGN_DEFAULT);
+}
+
+
+void *pmalloc(struct pmalloc_pool *pool, size_t size);
+
+
+/**
+ * pzalloc() - zero-initialized version of pmalloc()
+ * @pool: handle to the pool to be used for memory allocation
+ * @size: amount of memory (in bytes) requested
+ *
+ * Executes pmalloc(), initializing the memory requested to 0, before
+ * returning its address.
+ *
+ * Return:
+ * * pointer to the memory requested	- success
+ * * NULL				- error
+ */
+static inline void *pzalloc(struct pmalloc_pool *pool, size_t size)
+{
+	void *ptr = pmalloc(pool, size);
+
+	if (likely(ptr))
+		memset(ptr, 0, size);
+	return ptr;
+}
+
+
+/**
+ * pmalloc_array() - array version of pmalloc()
+ * @pool: handle to the pool to be used for memory allocation
+ * @n: number of elements in the array
+ * @size: amount of memory (in bytes) requested for each element
+ *
+ * Executes pmalloc(), on an array.
+ *
+ * Return:
+ * * the pmalloc result	- success
+ * * NULL		- error
+ */
+
+static inline void *pmalloc_array(struct pmalloc_pool *pool, size_t n,
+				  size_t size)
+{
+	if (unlikely(size != 0) && unlikely(n > SIZE_MAX / size))
+		return NULL;
+	return pmalloc(pool, n * size);
+}
+
+
+/**
+ * pcalloc() - array version of pzalloc()
+ * @pool: handle to the pool to be used for memory allocation
+ * @n: number of elements in the array
+ * @size: amount of memory (in bytes) requested for each element
+ *
+ * Executes pzalloc(), on an array.
+ *
+ * Return:
+ * * the pmalloc result	- success
+ * * NULL		- error
+ */
+static inline void *pcalloc(struct pmalloc_pool *pool, size_t n,
+			    size_t size)
+{
+	if (unlikely(size != 0) && unlikely(n > SIZE_MAX / size))
+		return NULL;
+	return pzalloc(pool, n * size);
+}
+
+
+/**
+ * pstrdup() - duplicate a string, using pmalloc()
+ * @pool: handle to the pool to be used for memory allocation
+ * @s: string to duplicate
+ *
+ * Generates a copy of the given string, allocating sufficient memory
+ * from the given pmalloc pool.
+ *
+ * Return:
+ * * pointer to the replica	- success
+ * * NULL			- error
+ */
+static inline char *pstrdup(struct pmalloc_pool *pool, const char *s)
+{
+	size_t len;
+	char *buf;
+
+	len = strlen(s) + 1;
+	buf = pmalloc(pool, len);
+	if (likely(buf))
+		strncpy(buf, s, len);
+	return buf;
+}
+
+
+void pmalloc_protect_pool(struct pmalloc_pool *pool);
+
+
+void pmalloc_destroy_pool(struct pmalloc_pool *pool);
+#endif
