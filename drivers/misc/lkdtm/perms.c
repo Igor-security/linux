ce06030c6725a4502734cba5acfd1dc1067c0bfc
Igor Stoppa <igor.stoppa@huawei.com>
2018-03-27 18:37:41 +0300

    lkdtm: crash on overwriting protected pmalloc var
    Verify that pmalloc read-only protection is in place: trying to
    overwrite a protected variable will crash the kernel.
    
    Signed-off-by: Igor Stoppa <igor.stoppa@huawei.com>

--- drivers/misc/lkdtm/perms.c
+++ drivers/misc/lkdtm/perms.c
@@ -9,6 +9,7 @@
 #include <linux/vmalloc.h>
 #include <linux/mman.h>
 #include <linux/uaccess.h>
+#include <linux/pmalloc.h>
 #include <asm/cacheflush.h>
 
 /* Whether or not to fill the target memory area with do_nothing(). */
@@ -103,6 +104,30 @@ void lkdtm_WRITE_RO_AFTER_INIT(void)
 	pr_info("attempting bad ro_after_init write at %p\n", ptr);
 	*ptr ^= 0xabcd1234;
 }
+
+#ifdef CONFIG_PROTECTABLE_MEMORY
+void lkdtm_WRITE_RO_PMALLOC(void)
+{
+	struct pmalloc_pool *pool;
+	int *i;
+
+	pool = pmalloc_create_pool();
+	if (WARN(!pool, "Failed preparing pool for pmalloc test."))
+		return;
+
+	i = pmalloc(pool, sizeof(int));
+	if (WARN(!i, "Failed allocating memory for pmalloc test.")) {
+		pmalloc_destroy_pool(pool);
+		return;
+	}
+
+	*i = INT_MAX;
+	pmalloc_protect_pool(pool);
+
+	pr_info("attempting bad pmalloc write at %p\n", i);
+	*i = 0;
+}
+#endif
 
 void lkdtm_WRITE_KERN(void)
 {
