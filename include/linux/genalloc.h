1b05923b364fc1f68ceb4115a9d359f8b4eff959
Igor Stoppa <igor.stoppa@huawei.com>
2018-04-27 04:11:51 +0400

    genalloc: track beginning of allocations
    The genalloc library is only capable of tracking if a certain unit of
    allocation is in use or not.
    
    It is not capable of discerning where the memory associated to an
    allocation request begins and where it ends.
    
    The reason is that units of allocations are tracked by using a bitmap,
    where each bit represents that the unit is either allocated (1) or
    available (0).
    
    The user of the API must keep track of how much space was requested, if
    it ever needs to be freed.
    
    This can cause errors being undetected.
    From a security standpoint, such errors could be exploited - or even
    forcibly caused - by an attacker, trying to obtain memory still in use
    by live pointer.
    
    Examples:
    * Only a subset of the memory provided to an allocation request is freed
    * The memory from a subsequent allocation is freed (potential attack)
    * The memory being freed doesn't start at the beginning of an
      allocation. (potential attack)
    
    The bitmap is used because it allows to perform lockless read/write
    access, where this is supported by hw through cmpxchg.
    Similarly, it is possible to scan the bitmap for a sufficiently long
    sequence of zeros, to identify zones available for allocation.
    
    This patch doubles the space reserved in the bitmap for each allocation,
    to track their beginning.
    
    For details, see the documentation inside lib/genalloc.c
    
    The primary effect of this patch is that code using the gen_alloc
    library does not need anymore to keep track of the size of the
    allocations it makes.
    
    Prior to this patch, it was necessary to keep track of the size of the
    allocation, so that it would be possible, later on, to know how much
    space should be freed.
    
    Now, users of the api can choose to etiher still specify explicitly the
    size, or let the library determine it, by giving a value of 0.
    
    However, even when the value is specified, the library still uses its on
    understanding of the space associated with a certain allocation, to
    confirm that they are consistent.
    
    This verification also confirms that the patch works correctly.
    
    Eventually, the extra parameter (and the corresponding verification)
    must be dropped, in favor of a simplified API.
    
    But this patch allows to transition them individually, while ensuring
    that the memory being freed has both a legal start and size.
    
    From the perspective of performance imapact, the patch doubles the size
    of the bitmap that must be parsed, however the lockless allocations is
    preserved.
    Regarding the actual amount of bits occupied, the current users of
    genalloc are of 2 main types and the size of one allocation unit in the
    bitmap corresponds respectively to the following:
    
    * SRAM / MURAM / small memory allocators, one allocation unit corresponds
    to either 1 or few bytes of special memory.
    However these kind of memory come in small sizes (few tens of kbytes),
    because they are often on-die and tend to take up space.
    Ex: 48kB SRAM, each byte mapped individually.
    48kB -> 6kB bitmap prepatch, 12kB bitmap after patch
    This type of memory is usually allocated during init or anyway at system
    startup and then used to preserve system status while the rest of the
    core/SoC is powered down.
    
    * DMA memory pools, one allocation unit corresponds to 1 page
    Ex: 1MB allocation request, with PAGE_SIZE = 4kB
    1MB -> 32B bitmap prepatch, 64B bitmap after patch.
    Assuming a cache line size of 64B, they would both fit in one.
    Also in this case, DMA buffers are typically established when either the
    system or a specific peripheral is brought up, but they are not
    continuously allocated and destroyed. Even if the allocation were to
    become slightly slower, it would not bring any significant performance
    loss.
    
    Furthermore, since the data in the bitmap is accessed sequentially, it
    should be prefetched for read, which means tha the increased time spent
    parsing the bitmap should be negligible, compared to the actual use of the
    memory being allocated.
    
    Signed-off-by: Igor Stoppa <igor.stoppa@huawei.com>

--- include/linux/genalloc.h
+++ include/linux/genalloc.h
@@ -32,7 +32,7 @@
 
 #include <linux/types.h>
 #include <linux/spinlock_types.h>
-#include <linux/atomic.h>
+#include <linux/slab.h>
 
 struct device;
 struct device_node;
@@ -76,7 +76,7 @@ struct gen_pool_chunk {
 	phys_addr_t phys_addr;		/* physical starting address of memory chunk */
 	unsigned long start_addr;	/* start address of memory chunk */
 	unsigned long end_addr;		/* end address of memory chunk (inclusive) */
-	unsigned long bits[0];		/* bitmap for allocating memory chunk */
+	unsigned long entries[0];	/* bitmap for allocating memory chunk */
 };
 
 /*
@@ -93,74 +93,82 @@ struct genpool_data_fixed {
 	unsigned long offset;		/* The offset of the specific region */
 };
 
-extern struct gen_pool *gen_pool_create(int, int);
-extern phys_addr_t gen_pool_virt_to_phys(struct gen_pool *pool, unsigned long);
-extern int gen_pool_add_virt(struct gen_pool *, unsigned long, phys_addr_t,
-			     size_t, int);
-/**
- * gen_pool_add - add a new chunk of special memory to the pool
- * @pool: pool to add new memory chunk to
- * @addr: starting address of memory chunk to add to pool
- * @size: size in bytes of the memory chunk to add to pool
- * @nid: node id of the node the chunk structure and bitmap should be
- *       allocated on, or -1
- *
- * Add a new chunk of special memory to the specified pool.
- *
- * Returns 0 on success or a -ve errno on failure.
- */
+struct gen_pool *gen_pool_create(int min_alloc_order, int nid);
+
+int gen_pool_add_virt(struct gen_pool *pool, unsigned long virt,
+		      phys_addr_t phys, size_t size, int nid);
+
+
 static inline int gen_pool_add(struct gen_pool *pool, unsigned long addr,
 			       size_t size, int nid)
 {
 	return gen_pool_add_virt(pool, addr, -1, size, nid);
 }
-extern void gen_pool_destroy(struct gen_pool *);
-extern unsigned long gen_pool_alloc(struct gen_pool *, size_t);
-extern unsigned long gen_pool_alloc_algo(struct gen_pool *, size_t,
-		genpool_algo_t algo, void *data);
-extern void *gen_pool_dma_alloc(struct gen_pool *pool, size_t size,
-		dma_addr_t *dma);
-extern void gen_pool_free(struct gen_pool *, unsigned long, size_t);
-extern void gen_pool_for_each_chunk(struct gen_pool *,
-	void (*)(struct gen_pool *, struct gen_pool_chunk *, void *), void *);
-extern size_t gen_pool_avail(struct gen_pool *);
-extern size_t gen_pool_size(struct gen_pool *);
 
-extern void gen_pool_set_algo(struct gen_pool *pool, genpool_algo_t algo,
-		void *data);
+phys_addr_t gen_pool_virt_to_phys(struct gen_pool *pool, unsigned long addr);
 
-extern unsigned long gen_pool_first_fit(unsigned long *map, unsigned long size,
-		unsigned long start, unsigned int nr, void *data,
-		struct gen_pool *pool);
+void gen_pool_destroy(struct gen_pool *pool);
 
-extern unsigned long gen_pool_fixed_alloc(unsigned long *map,
-		unsigned long size, unsigned long start, unsigned int nr,
-		void *data, struct gen_pool *pool);
+unsigned long gen_pool_alloc(struct gen_pool *pool, size_t size);
 
-extern unsigned long gen_pool_first_fit_align(unsigned long *map,
-		unsigned long size, unsigned long start, unsigned int nr,
-		void *data, struct gen_pool *pool);
+unsigned long gen_pool_alloc_algo(struct gen_pool *pool, size_t size,
+				  genpool_algo_t algo, void *data);
+
+void *gen_pool_dma_alloc(struct gen_pool *pool, size_t size, dma_addr_t *dma);
 
 
-extern unsigned long gen_pool_first_fit_order_align(unsigned long *map,
-		unsigned long size, unsigned long start, unsigned int nr,
-		void *data, struct gen_pool *pool);
-
-extern unsigned long gen_pool_best_fit(unsigned long *map, unsigned long size,
-		unsigned long start, unsigned int nr, void *data,
-		struct gen_pool *pool);
+void gen_pool_free(struct gen_pool *pool, unsigned long addr, size_t size);
 
 
-extern struct gen_pool *devm_gen_pool_create(struct device *dev,
-		int min_alloc_order, int nid, const char *name);
-extern struct gen_pool *gen_pool_get(struct device *dev, const char *name);
+void gen_pool_for_each_chunk(struct gen_pool *pool,
+			     void (*func)(struct gen_pool *pool,
+					  struct gen_pool_chunk *chunk,
+					  void *data),
+			     void *data);
 
 bool addr_in_gen_pool(struct gen_pool *pool, unsigned long start,
-			size_t size);
+		      size_t size);
+
+size_t gen_pool_avail(struct gen_pool *pool);
+
+size_t gen_pool_size(struct gen_pool *pool);
+
+void gen_pool_set_algo(struct gen_pool *pool, genpool_algo_t algo, void *data);
+
+unsigned long gen_pool_first_fit(unsigned long *map, unsigned long size,
+				 unsigned long start, unsigned int nr,
+				 void *data, struct gen_pool *pool);
+
+
+unsigned long gen_pool_first_fit_align(unsigned long *map,
+				       unsigned long size,
+				       unsigned long start,
+				       unsigned int nr, void *data,
+				       struct gen_pool *pool);
+
+unsigned long gen_pool_fixed_alloc(unsigned long *map, unsigned long size,
+				   unsigned long start, unsigned int nr,
+				   void *data, struct gen_pool *pool);
+
+
+unsigned long gen_pool_first_fit_order_align(unsigned long *map,
+					     unsigned long size,
+					     unsigned long start,
+					     unsigned int nr, void *data,
+					     struct gen_pool *pool);
+
+unsigned long gen_pool_best_fit(unsigned long *map, unsigned long size,
+				unsigned long start, unsigned int nr,
+				void *data, struct gen_pool *pool);
+
+struct gen_pool *gen_pool_get(struct device *dev, const char *name);
+
+struct gen_pool *devm_gen_pool_create(struct device *dev, int min_alloc_order,
+				      int nid, const char *name);
 
 #ifdef CONFIG_OF
-extern struct gen_pool *of_gen_pool_get(struct device_node *np,
-	const char *propname, int index);
+struct gen_pool *of_gen_pool_get(struct device_node *np,
+				 const char *propname, int index);
 #else
 static inline struct gen_pool *of_gen_pool_get(struct device_node *np,
 	const char *propname, int index)
