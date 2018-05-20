6c9cd4dfdf20d0bba8babd8ab311005b824750a4
Igor Stoppa <igor.stoppa@huawei.com>
2018-05-20 15:05:59 +0300

    Rare Write support for statically allocated memory
    Support for allocating static memory that is normally read-only and can
    be altered only by using specific primitives.
    
    Static vairables declared with the __rare_write_after_init modifier are
    normally writable until the completion of the init phase.
    
    Afterwards, they can only be altered using a function of the rare_write
    family.
    
    To minimize both accidental and malicious abuse of these functions, they
    are inlined and they operate exclusively within a data segment, where
    all the related data is allocated.
    
    Reading this sort of data doesn't cause any of penalty, however the
    write operation is intrinsically slower, due to the write mechanism.
    
    When data in a page must be altered, the mapping of the page is not
    affected, but the page is remapped to a new temporary random address, with
    write permission.
    
    This is meant to avoid that an attacker, busy looping on another core or
    thread, can exploit a window of opportunity where the original mapping
    would become writeable.
    
    The attacker might not be interested in altering the data beign modified
    by the OS, but rather, some other data which happens to be located in
    the same memory page.
    
    Of course the attack can still happen on the temporary mapping, but the
    attacker needs to figure out where it is.
    Ideally, every other core would be disabled during the write operation,
    without a need for the remapping, however this would most likely cause
    unacceptable freeze in the system.
    
    Alternatively, the attack could be performed by carefully timing an
    interrupt on the core executing the write operation at the remapped
    address.
    The attacker would freeze the writable mapping and have more time to
    locate it. For this reason, local interrupts are disabled before the
    temporary mapping is created, until it is destroyed.
    The downside of this approach is that in the worst case an interrupt will
    be delayed for the duration of a remapping/write/unmap cycle.
    To provide an upper bound to such delay, one page at a time is altered,
    re-enabling interrupts inbetween cycles, when the temporary mappings are
    not available.
    
    Such approach is sub-optimal from the perspective of altering large
    amounts of data, which could be remapped in larger portions, however the
    functionality is meant for data that is altered seldomly and can
    tolerate somewhat slow updates.
    
    To ensure integrity when writing multi-byte data that might cause
    problems if not modified atomically (ex: pointers), write_once is used,
    however there might be problems if used with packed structures, where
    the data alignment is not guarranteed. In these cases it might be
    necessary to use some form of locking between readers and writers, even
    if there is only one writer.
    
    Regarding concurrent rare_write calls that affect non-overlapping data
    ranges, they can happen simultaneously, each at its own remapped
    address, as long as the HW cache coherency guarrantees, at physical
    level, that the symultaneous writes will be handled correctly.
    
    Signed-off-by: Igor Stoppa <igor.stoppa@huawei.com>
    CC: Ashish Raj <araj@elisanet.fi>
    CC: Carlos Chinea Perez <carlos.chinea.perez@huawei.com>
    CC: Remi Denis Courmont <remi.denis.courmont@huawei.com>

---
+++ include/linux/rare_write.h
@@ -0,0 +1,267 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * rare_write.h: Header for rare writes to statically allocated variables
+ *
+ * (C) Copyright 2018 Huawei Technologies Co. Ltd.
+ * Author: Igor Stoppa <igor.stoppa@huawei.com>
+ *
+ * Support for modifying "variables" residing in protected memory.
+ * They are write-protected against direct writes and can be altered only
+ * through special means.
+ */
+
+#ifndef _LINUX_RARE_WRITE_H
+#define _LINUX_RARE_WRITE_H
+
+#include <linux/mm.h>
+#include <linux/vmalloc.h>
+#include <linux/compiler.h>
+#include <linux/irqflags.h>
+
+extern long __start_rare_write_after_init;
+extern long __end_rare_write_after_init;
+
+/**
+ * rare_write_check_boundaries - verifies range correctness
+ * @dst: beginning of the memory to alter
+ * @size: amount of bytes to alter
+ *
+ * Returns true on range ok, false on error.
+ *
+ * This test is meant to verify that the range beign altered is within the
+ * memory segment reserved for re-writable memory and doesn't overlap any
+ * other area.
+ */
+static __always_inline
+bool rare_write_check_boundaries(const void *dst, size_t size)
+{
+	size_t start = (size_t)&__start_rare_write_after_init;
+	size_t end = (size_t)&__end_rare_write_after_init;
+	size_t low = (size_t)dst;
+	size_t high = (size_t)dst + size;
+
+	return likely(start <= low && low < high && high <= end);
+}
+
+/*
+ * This is the core of the rare write functionality.
+ * It doesn't perform any check on the validity of the target.
+ * The wrapper using it is supposed to apply sensible verification
+ * criteria.
+ */
+static __always_inline
+bool __raw_rare_write(const void *dst, const void *const src, size_t n_bytes)
+{
+	size_t size;
+	unsigned long flags;
+	const void *d = dst;
+	const void *s = src;
+
+	while (n_bytes) {
+		struct page *page;
+		void *base;
+		unsigned long offset;
+		size_t offset_complement;
+
+		local_irq_save(flags);
+		page = virt_to_page(d);
+		offset = (unsigned long)d & ~PAGE_MASK;
+		offset_complement = ((size_t)PAGE_SIZE) - offset;
+		size = min(((int)n_bytes), ((int)offset_complement));
+		base = vmap(&page, 1, VM_MAP, PAGE_KERNEL);
+		if (WARN(!base, "failed to remap rare-write page"))
+			goto err;
+		__write_once_size(base + offset, (void *)s, size);
+		vunmap(base);
+		d += size;
+		s += size;
+		n_bytes -= size;
+		local_irq_restore(flags);
+	}
+	return true;
+err:
+	local_irq_restore(flags);
+	return false;
+}
+
+/**
+ * rare_write_array - copies n bytes from source to destination
+ * @dst: beginning of the memory to copy to
+ * @src: beginning of the memory to copy from
+ * @size: amount of bytes to copy
+ *
+ * Returns true on success, false otherwise.
+ */
+static __always_inline
+bool rare_write_array(const void *dst, const void *src, size_t n_bytes)
+{
+
+	if (WARN(!rare_write_check_boundaries(dst, n_bytes),
+		 "Not a valid rare_write destination."))
+		return false;
+	return __raw_rare_write(dst, src, n_bytes);
+
+}
+
+#define __rare_write_simple(dst_ptr, src_ptr)				\
+	rare_write_array(dst_ptr, src_ptr, sizeof(*(src_ptr)))
+
+#define __rare_write_safe(dst_ptr, src_ptr,				\
+			  unique_dst_ptr, unique_src_ptr)		\
+({									\
+	typeof(dst_ptr) unique_dst_ptr = (dst_ptr);			\
+	typeof(src_ptr) unique_src_ptr = (src_ptr);			\
+									\
+	rare_write_array(unique_dst_ptr, unique_src_ptr,		\
+		     sizeof(*(unique_src_ptr)));			\
+})
+
+#define __safe_ops(dst, src)	\
+	(__typecheck(dst, src) && __no_side_effects(dst, src))
+
+/**
+ * rare_write - copies an object over another of same type and size
+ * @dst_ptr: address of the destination object
+ * @src_ptr: address of the source object
+ */
+#define rare_write(dst_ptr, src_ptr)					\
+	__builtin_choose_expr(__safe_ops(dst_ptr, src_ptr),		\
+			      __rare_write_simple(dst_ptr, src_ptr),	\
+			      __rare_write_safe(dst_ptr, src_ptr,	\
+						__UNIQUE_ID(__dst_ptr),	\
+						__UNIQUE_ID(__src_ptr)))
+
+/* Specialized versions of rare write */
+
+/**
+ * rare_write_ptr - alters a pointer in rare-write memory
+ * @dst: target for write
+ * @val: new value
+ *
+ * Returns true on success, false otherwise.
+ */
+static __always_inline
+bool rare_write_ptr(const void *dst, const void *val)
+{
+	return rare_write_array(dst, &val, sizeof(val));
+}
+
+/**
+ * rare_write_char - alters a char in rare-write memory
+ * @dst: target for write
+ * @val: new value
+ *
+ * Returns true on success, false otherwise.
+ */
+static __always_inline
+bool rare_write_char(const char *dst, const char val)
+{
+	return rare_write_array(dst, &val, sizeof(val));
+}
+
+/**
+ * rare_write_short - alters a short in rare-write memory
+ * @dst: target for write
+ * @val: new value
+ *
+ * Returns true on success, false otherwise.
+ */
+static __always_inline
+bool rare_write_short(const short *dst, const short val)
+{
+	return rare_write_array(dst, &val, sizeof(val));
+}
+
+/**
+ * rare_write_ushort - alters an unsigned short in rare-write memory
+ * @dst: target for write
+ * @val: new value
+ *
+ * Returns true on success, false otherwise.
+ */
+static __always_inline
+bool rare_write_ushort(const unsigned short *dst, const unsigned short val)
+{
+	return rare_write_array(dst, &val, sizeof(val));
+}
+
+/**
+ * rare_write_int - alters an int in rare-write memory
+ * @dst: target for write
+ * @val: new value
+ *
+ * Returns true on success, false otherwise.
+ */
+static __always_inline
+bool rare_write_int(const int *dst, const int val)
+{
+	return rare_write_array(dst, &val, sizeof(val));
+}
+
+/**
+ * rare_write_uint - alters an unsigned int in rare-write memory
+ * @dst: target for write
+ * @val: new value
+ *
+ * Returns true on success, false otherwise.
+ */
+static __always_inline
+bool rare_write_uint(const unsigned int *dst, const unsigned int val)
+{
+	return rare_write_array(dst, &val, sizeof(val));
+}
+
+/**
+ * rare_write_long - alters a long in rare-write memory
+ * @dst: target for write
+ * @val: new value
+ *
+ * Returns true on success, false otherwise.
+ */
+static __always_inline
+bool rare_write_long(const long *dst, const long val)
+{
+	return rare_write_array(dst, &val, sizeof(val));
+}
+
+/**
+ * rare_write_ulong - alters an unsigned long in rare-write memory
+ * @dst: target for write
+ * @val: new value
+ *
+ * Returns true on success, false otherwise.
+ */
+static __always_inline
+bool rare_write_ulong(const unsigned long *dst, const unsigned long val)
+{
+	return rare_write_array(dst, &val, sizeof(val));
+}
+
+/**
+ * rare_write_longlong - alters a long long in rare-write memory
+ * @dst: target for write
+ * @val: new value
+ *
+ * Returns true on success, false otherwise.
+ */
+static __always_inline
+bool rare_write_longlong(const long long *dst, const long long val)
+{
+	return rare_write_array(dst, &val, sizeof(val));
+}
+
+/**
+ * rare_write_ulonglong - alters an unsigned long long in rare-write memory
+ * @dst: target for write
+ * @val: new value
+ *
+ * Returns true on success, false otherwise.
+ */
+static __always_inline
+bool rare_write_ulonglong(const unsigned long long *dst,
+			  const unsigned long long val)
+{
+	return rare_write_array(dst, &val, sizeof(val));
+}
+
+#endif
