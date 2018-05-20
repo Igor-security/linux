26b424703c17b7b0e1a0144958ecdf8bb4455d86
Igor Stoppa <igor.stoppa@huawei.com>
2018-05-20 21:48:59 +0300

    Doubly Linked List using protected memory
    In some cases, all the data needing protection can be allocated from a pool
    in one go, as directly writable, then initialized and protected.
    The sequence is relatively short and it's acceptable to leave the entire
    data set unprotected.
    
    In other cases, this is not possible, because the data will trickle over
    a relatively long period of time, in a non predictable way, possibly for
    the entire duration of hte operations.
    
    For these cases, the safe approach is to have the memory already write
    protected, when allocated. However, this will require replacing any
    direct assignment with calls to rare_write().
    
    The usual doubly linked list needs, therefore, to be replaced with a
    derived variant that will replace direct writes, with rare_write().
    
    This patch implements the most basic functionality.
    
    Signed-off-by: Igor Stoppa <igor.stoppa@huawei.com>

---
+++ include/linux/prlist.h
@@ -0,0 +1,139 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * prlist.h: Header for Protected Doubly Linked List
+ *
+ * (C) Copyright 2018 Huawei Technologies Co. Ltd.
+ * Author: Igor Stoppa <igor.stoppa@huawei.com>
+ */
+
+#ifndef _LINUX_PRLIST_H
+#define _LINUX_PRLIST_H
+
+#include <linux/list.h>
+#include <linux/kernel.h>
+#include <linux/pmalloc.h>
+
+/* The order inside the union is important, to support LIST_HEAD_INIT() */
+struct prlist_head {
+	union {
+		struct {
+			struct prlist_head *next, *prev;
+		};
+		struct list_head list;
+	};
+};
+
+struct prhlist_head {
+	union {
+		struct prhlist_node *first;
+		struct hlist_head head;
+	};
+};
+
+struct prhlist_node {
+	union{
+		struct {
+			struct prhlist_node *next, **pprev;
+		};
+		struct hlist_node node;;
+	};
+};
+
+static __always_inline
+struct pmalloc_pool *prlist_create_custom_pool(size_t refill,
+					       unsigned short align_order)
+{
+	return pmalloc_create_custom_pool(refill, align_order,
+					  PMALLOC_AUTO_RW);
+}
+
+static __always_inline
+struct pmalloc_pool *prlist_create_pool(void)
+{
+	return prlist_create_custom_pool(PMALLOC_REFILL_DEFAULT,
+					 PMALLOC_ALIGN_ORDER_DEFAULT);
+}
+
+static __always_inline
+void *prlist_alloc(struct pmalloc_pool *pool, size_t size)
+{
+	return pmalloc(pool, size);
+}
+
+static __always_inline
+void prlist_set_prev(struct pmalloc_pool *pool, struct prlist_head *head,
+		     const struct prlist_head *prev)
+{
+	if (unlikely(rare_write_check_boundaries(head, sizeof(head))))
+		rare_write_ptr(&head->prev, prev);
+	else
+		pmalloc_rare_write_ptr(pool, &head->prev, prev);
+}
+
+static __always_inline
+void prlist_set_next(struct pmalloc_pool *pool, struct prlist_head *head,
+		     const struct prlist_head *next)
+{
+	if (unlikely(rare_write_check_boundaries(head, sizeof(head))))
+		rare_write_ptr(&head->next, next);
+	else
+		pmalloc_rare_write_ptr(pool, &head->next, next);
+}
+
+static __always_inline
+void INIT_PRLIST_HEAD(struct pmalloc_pool *pool, struct prlist_head *head)
+{
+	prlist_set_prev(pool, head, head);
+	prlist_set_next(pool, head, head);
+}
+
+static __always_inline
+void INIT_STATIC_PRLIST_HEAD(struct prlist_head *head)
+{
+	INIT_PRLIST_HEAD(NULL, head);
+}
+
+static __always_inline
+void prlist_add(struct pmalloc_pool *pool, struct prlist_head *new,
+		struct prlist_head *head)
+{
+	prlist_set_next(pool, new, head->next);
+	prlist_set_prev(pool, new, head);
+	prlist_set_prev(pool, head->next, new);
+	prlist_set_next(pool, head, new);
+}
+
+static __always_inline
+void prlist_add_tail(struct pmalloc_pool *pool, struct prlist_head *new,
+		     struct prlist_head *head)
+{
+	prlist_set_next(pool, new, head);
+	prlist_set_prev(pool, new, head->prev);
+	prlist_set_next(pool, head->prev, new);
+	prlist_set_prev(pool, head, new);
+}
+
+static __always_inline
+void prlist_del_entry(struct pmalloc_pool *pool, struct prlist_head *entry)
+{
+	prlist_set_prev(pool, entry->next, entry->prev);
+	prlist_set_next(pool, entry->prev, entry->next);
+	pmalloc_rare_write_ptr(pool, &entry->next, LIST_POISON1);
+	pmalloc_rare_write_ptr(pool, &entry->prev, LIST_POISON2);
+}
+
+static __always_inline void dump_prlist_head(struct prlist_head *head)
+{
+	pr_info("head: 0x%08lx   prev: 0x%08lx   next: 0x%08lx",
+		(unsigned long)head, (unsigned long)head->prev,
+		(unsigned long)head->next);
+}
+
+static __always_inline void prlist_destroy_pool(struct pmalloc_pool *pool)
+{
+	pmalloc_destroy_pool(pool);
+}
+
+#define PRHLIST_HEAD_INIT {.head = HLIST_HEAD_INIT}
+
+#endif
