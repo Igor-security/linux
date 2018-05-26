09b75501a6dbb3c0abb962d53c6911a6cbd3dba7
Igor Stoppa <igor.stoppa@huawei.com>
2018-05-26 12:40:15 +0300

    prlist wip

--- lib/prlist.c
+++ lib/prlist.c
@@ -1,23 +1,23 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * prot_list.c: protected double linked list
+ * prlist.c: Protected Double Linked List
  *
  * (C) Copyright 2018 Huawei Technologies Co. Ltd.
  * Author: Igor Stoppa <igor.stoppa@huawei.com>
  */
 
-#include <linux/prot_list.h>
+#include <linux/prlist.h>
 
-struct prot_list_pool *prot_list_create_custom_pool(size_t refill,
-						    unsigned short align_order)
+struct prlist_pool *prlist_create_custom_pool(size_t refill,
+					      unsigned short align_order)
 {
-	struct prot_list_pool *pool;
+	struct prlist_pool *pool;
 
-	pool = kzalloc(sizeof(struct prot_list_pool), GFP_KERNEL);
+	pool = kzalloc(sizeof(struct prlist_pool), GFP_KERNEL);
 	if (WARN(!pool, "Could not allocate pool meta data."))
 		return NULL;
 	pmalloc_init_custom_pool(&pool->pool, refill, align_order,
 				 PMALLOC_AUTO_RW);
 	return pool;
 }
-EXPORT_SYMBOL(prot_list_create_custom_pool);
+EXPORT_SYMBOL(prlist_create_custom_pool);
