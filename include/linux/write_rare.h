a0ea5e2e881d342033cbd9e36fd85017452f411b
Igor Stoppa <igor.stoppa@huawei.com>
2018-08-12 05:33:51 +0300

    Extend write rare to vmap_areas
    Add support to __raw_wr() for modifying also virtually contiguous memory
    provided by vmalloc.
    Since __raw_wr() doesn't perform any check on the destination
    range, the caller *must* take care of validating the inputs.
    
    Signed-off-by: Igor Stoppa <igor.stoppa@huawei.com>

--- include/linux/write_rare.h
+++ include/linux/write_rare.h
@@ -13,49 +13,67 @@
 #ifndef _LINUX_WRITE_RARE_H
 #define _LINUX_WRITE_RARE_H
 
+#include <linux/set_memory.h>
 #include <linux/mm.h>
 #include <linux/vmalloc.h>
+#include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/mutex.h>
 #include <linux/compiler.h>
 #include <linux/irqflags.h>
 
+
+/* ===================== Write Rare Functionality ===================== */
+
+/*
+ * The following two variables are satically allocated by the linker script
+ * at the the boundaries of the memory region (rounded up to multiples of
+ * PAGE_SIZE) reserved for __wr_after_init.
+ */
 extern long __start_wr_after_init;
 extern long __end_wr_after_init;
 
-/**
- * wr_check_boundaries - verifies range correctness
- * @dst: beginning of the memory to alter
- * @size: amount of bytes to alter
- *
- * Returns true on range ok, false on error.
- *
- * This test is meant to verify that the range beign altered is within the
- * memory segment reserved for re-writable memory and doesn't overlap any
- * other area.
- */
-static __always_inline
-bool wr_check_boundaries(const void *dst, size_t size)
+static __always_inline bool __is_wr_after_init(const void *ptr, size_t size)
 {
 	size_t start = (size_t)&__start_wr_after_init;
 	size_t end = (size_t)&__end_wr_after_init;
-	size_t low = (size_t)dst;
-	size_t high = (size_t)dst + size;
+	size_t low = (size_t)ptr;
+	size_t high = (size_t)ptr + size;
 
 	return likely(start <= low && low < high && high <= end);
 }
 
-/*
- * This is the core of the write rare memset functionality.
- * It doesn't perform any check on the validity of the target.
- * The wrapper using it is supposed to apply sensible verification
- * criteria.
+static __always_inline bool __is_wr_pool(const void *ptr, size_t size)
+{
+	struct vmap_area *area;
+
+	if (!is_vmalloc_addr(ptr))
+		return false;
+	area = find_vmap_area((unsigned long)ptr);
+	return area && area->vm && (area->vm->size >= size) &&
+		((area->vm->flags & (VM_PMALLOC | VM_PMALLOC_WR)) ==
+		 (VM_PMALLOC | VM_PMALLOC_WR));
+}
+
+/**
+ * wr_memset - sets n bytes of the destination to a specified value,
+ *             within the boundaries of the __wr_after_init segment
+ * @dst: beginning of the memory to write to
+ * @c: byte to replicate
+ * @size: amount of bytes to copy
+ *
+ * Returns true on success, false otherwise.
  */
 static __always_inline
-bool __raw_wr_memset(const void *dst, const int c, size_t n_bytes)
+bool wr_memset(const void *dst, const int c, size_t n_bytes)
 {
 	size_t size;
 	unsigned long flags;
 	const void *d = dst;
+	bool is_virt = __is_wr_after_init(dst, n_bytes);
 
+	if (!(is_virt || likely(__is_wr_pool(dst, n_bytes))))
+		return !WARN_ON("Write rare on invalid memory range.");
 	while (n_bytes) {
 		struct page *page;
 		void *base;
@@ -63,7 +81,7 @@ bool __raw_wr_memset(const void *dst, const int c, size_t n_bytes)
 		size_t offset_complement;
 
 		local_irq_save(flags);
-		page = virt_to_page(d);
+		page = is_virt ? virt_to_page(d) : vmalloc_to_page(d);
 		offset = (unsigned long)d & ~PAGE_MASK;
 		offset_complement = ((size_t)PAGE_SIZE) - offset;
 		size = min(((int)n_bytes), ((int)offset_complement));
@@ -82,38 +100,17 @@ err:
 	return false;
 }
 
-/**
- * wr_memset - sets n bytes of the destination to a specified value,
- *             within the boundaries of the __wr_after_init segment
- * @dst: beginning of the memory to write to
- * @c: byte to replicate
- * @size: amount of bytes to copy
- *
- * Returns true on success, false otherwise.
- */
 static __always_inline
-bool wr_memset(const void *dst, const int c, size_t n_bytes)
-{
-	if (WARN(!wr_check_boundaries(dst, n_bytes),
-		 "Not a valid write rare destination."))
-		return false;
-	return __raw_wr_memset(dst, c, n_bytes);
-}
-
-/*
- * This is the core of the write rare copy functionality.
- * It doesn't perform any check on the validity of the target.
- * The wrapper using it is supposed to apply sensible verification
- * criteria.
- */
-static __always_inline
-bool __raw_wr_copy(const void *dst, const void *const src, size_t n_bytes)
+bool wr_memcpy(const void *dst, const void *src, size_t n_bytes)
 {
 	size_t size;
 	unsigned long flags;
 	const void *d = dst;
 	const void *s = src;
+	bool is_virt = __is_wr_after_init(dst, n_bytes);
 
+	if (!(is_virt || likely(__is_wr_pool(dst, n_bytes))))
+		return !WARN_ON("Write rare on invalid memory range.");
 	while (n_bytes) {
 		struct page *page;
 		void *base;
@@ -121,7 +118,7 @@ bool __raw_wr_copy(const void *dst, const void *const src, size_t n_bytes)
 		size_t offset_complement;
 
 		local_irq_save(flags);
-		page = virt_to_page(d);
+		page = is_virt ? virt_to_page(d) : vmalloc_to_page(d);
 		offset = (unsigned long)d & ~PAGE_MASK;
 		offset_complement = ((size_t)PAGE_SIZE) - offset;
 		size = min(((int)n_bytes), ((int)offset_complement));
@@ -141,26 +138,8 @@ err:
 	return false;
 }
 
-/**
- * wr_copy - copies n bytes from source to destination, within the
- *            boundaries of the __wr_after_init segment
- * @dst: beginning of the memory to copy to
- * @src: beginning of the memory to copy from
- * @size: amount of bytes to copy
- *
- * Returns true on success, false otherwise.
- */
-static __always_inline
-bool wr_copy(const void *dst, const void *src, size_t n_bytes)
-{
-	if (WARN(!wr_check_boundaries(dst, n_bytes),
-		 "Not a valid write rare destination."))
-		return false;
-	return __raw_wr_copy(dst, src, n_bytes);
-}
-
 #define __wr_simple(dst_ptr, src_ptr)					\
-	wr_copy(dst_ptr, src_ptr, sizeof(*(src_ptr)))
+	wr_memcpy(dst_ptr, src_ptr, sizeof(*(src_ptr)))
 
 #define __wr_safe(dst_ptr, src_ptr,					\
 		  unique_dst_ptr, unique_src_ptr)			\
@@ -168,8 +147,8 @@ bool wr_copy(const void *dst, const void *src, size_t n_bytes)
 	typeof(dst_ptr) unique_dst_ptr = (dst_ptr);			\
 	typeof(src_ptr) unique_src_ptr = (src_ptr);			\
 									\
-	wr_copy(unique_dst_ptr, unique_src_ptr,			\
-		     sizeof(*(unique_src_ptr)));			\
+	wr_memcpy(unique_dst_ptr, unique_src_ptr,			\
+		  sizeof(*(unique_src_ptr)));				\
 })
 
 #define __safe_ops(dst, src)	\
@@ -187,7 +166,7 @@ bool wr_copy(const void *dst, const void *src, size_t n_bytes)
 						__UNIQUE_ID(__dst_ptr),	\
 						__UNIQUE_ID(__src_ptr)))
 
-/* Specialized versions of write rare */
+/* ---------------- Specialized versions of write rare ---------------- */
 
 /**
  * wr_ptr - alters a pointer in write rare memory
@@ -199,7 +178,7 @@ bool wr_copy(const void *dst, const void *src, size_t n_bytes)
 static __always_inline
 bool wr_ptr(const void *dst, const void *val)
 {
-	return wr_copy(dst, &val, sizeof(val));
+	return wr_memcpy(dst, &val, sizeof(val));
 }
 
 /**
@@ -212,7 +191,7 @@ bool wr_ptr(const void *dst, const void *val)
 static __always_inline
 bool wr_char(const char *dst, const char val)
 {
-	return wr_copy(dst, &val, sizeof(val));
+	return wr_memcpy(dst, &val, sizeof(val));
 }
 
 /**
@@ -225,7 +204,7 @@ bool wr_char(const char *dst, const char val)
 static __always_inline
 bool wr_short(const short *dst, const short val)
 {
-	return wr_copy(dst, &val, sizeof(val));
+	return wr_memcpy(dst, &val, sizeof(val));
 }
 
 /**
@@ -238,7 +217,7 @@ bool wr_short(const short *dst, const short val)
 static __always_inline
 bool wr_ushort(const unsigned short *dst, const unsigned short val)
 {
-	return wr_copy(dst, &val, sizeof(val));
+	return wr_memcpy(dst, &val, sizeof(val));
 }
 
 /**
@@ -251,7 +230,7 @@ bool wr_ushort(const unsigned short *dst, const unsigned short val)
 static __always_inline
 bool wr_int(const int *dst, const int val)
 {
-	return wr_copy(dst, &val, sizeof(val));
+	return wr_memcpy(dst, &val, sizeof(val));
 }
 
 /**
@@ -264,7 +243,7 @@ bool wr_int(const int *dst, const int val)
 static __always_inline
 bool wr_uint(const unsigned int *dst, const unsigned int val)
 {
-	return wr_copy(dst, &val, sizeof(val));
+	return wr_memcpy(dst, &val, sizeof(val));
 }
 
 /**
@@ -277,7 +256,7 @@ bool wr_uint(const unsigned int *dst, const unsigned int val)
 static __always_inline
 bool wr_long(const long *dst, const long val)
 {
-	return wr_copy(dst, &val, sizeof(val));
+	return wr_memcpy(dst, &val, sizeof(val));
 }
 
 /**
@@ -290,7 +269,7 @@ bool wr_long(const long *dst, const long val)
 static __always_inline
 bool wr_ulong(const unsigned long *dst, const unsigned long val)
 {
-	return wr_copy(dst, &val, sizeof(val));
+	return wr_memcpy(dst, &val, sizeof(val));
 }
 
 /**
@@ -303,7 +282,7 @@ bool wr_ulong(const unsigned long *dst, const unsigned long val)
 static __always_inline
 bool wr_longlong(const long long *dst, const long long val)
 {
-	return wr_copy(dst, &val, sizeof(val));
+	return wr_memcpy(dst, &val, sizeof(val));
 }
 
 /**
@@ -317,7 +296,7 @@ static __always_inline
 bool wr_ulonglong(const unsigned long long *dst,
 			  const unsigned long long val)
 {
-	return wr_copy(dst, &val, sizeof(val));
+	return wr_memcpy(dst, &val, sizeof(val));
 }
 
 #endif
