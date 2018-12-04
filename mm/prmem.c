1484a7e4934bb8ad6a5e9df0c26e30c433151acc
Igor Stoppa <igor.stoppa@huawei.com>
2018-12-04 12:44:11 +0200

    __wr_after_init: remap the whole range
    Create a permanent writable mapping for the whole range of pages that
    are __wr_after_init.
    
    Signed-off-by: Igor Stoppa <igor.stoppa@huawei.com>

--- mm/prmem.c
+++ mm/prmem.c
@@ -6,7 +6,6 @@
  * Author: Igor Stoppa <igor.stoppa@huawei.com>
  */
 
-//#include <linux/set_memory.h>
 #include <linux/mm.h>
 #include <linux/string.h>
 #include <linux/compiler.h>
@@ -15,12 +14,9 @@
 #include <linux/rcupdate.h>
 #include <linux/prmem.h>
 
-const char WR_ERR_RANGE_MSG[] = "Write rare on invalid memory range.";
-const char WR_ERR_PAGE_MSG[] = "Failed to remap write rare page.";
-
 static __ro_after_init bool wr_ready;
 static __ro_after_init struct mm_struct *wr_poking_mm;
-static __ro_after_init unsigned long wr_poking_addr;
+static __ro_after_init unsigned long wr_poking_base;
 
 /*
  * The following two variables are statically allocated by the linker
@@ -37,134 +33,93 @@ static inline bool is_wr_after_init(unsigned long ptr, __kernel_size_t size)
 	unsigned long low = ptr;
 	unsigned long high = ptr + size;
 
-	return likely(start <= low && low < high && high <= end);
+	return likely(start <= low && low <= high && high <= end);
 }
 
-static inline
-void op_start(unsigned long dst, unsigned long *psrc, __kernel_size_t len,
-		enum wr_op_type op)
-{
-	if (op == WR_MEMCPY)
-		memcpy((void *)dst, (void *)*psrc, len);
-	if (op == WR_MEMSET)
-		memset((u8 *)dst, (u8)*psrc, len);
-	if (op == WR_RCU_ASSIGN_PTR)
-		/*
-		 * Use the version of rcu_assign_pointer not optimized
-		 * for constants
-		 */
-		smp_store_release((void **)dst,
-				  RCU_INITIALIZER((void **)*psrc));
-}
-
-static inline
-void op_end(unsigned long dst, unsigned long *psrc, __kernel_size_t len,
-	      enum wr_op_type op)
-{
-	if (op == WR_MEMCPY) {
-		BUG_ON(memcmp((void *)dst, (void *)*psrc, len));
-		*psrc += len;
-	} else if (op == WR_MEMSET) {
-		BUG_ON(memtst((void *)dst, (int)*psrc, len));
-	} else if (op == WR_RCU_ASSIGN_PTR) {
-		BUG_ON(*(unsigned long *)dst != *psrc);
-	}
-}
-
-static inline
-void op_page(unsigned long dst, unsigned long *psrc,
-	     __kernel_size_t len, enum wr_op_type op)
-{
-	temporary_mm_state_t prev;
-	unsigned long flags;
-	pte_t pte, *ptep;
-	spinlock_t *ptl;
-	struct page *page = virt_to_page(dst);
-
-	BUG_ON(!page);
-	local_irq_save(flags);
-
-	/* The lock is not needed, but avoids open-coding. */
-	ptep = get_locked_pte(wr_poking_mm, wr_poking_addr, &ptl);
-	VM_BUG_ON(!ptep); /* preallocated in poking_init() */
-
-	pte = mk_pte(page, PAGE_KERNEL);
-	set_pte_at(wr_poking_mm, wr_poking_addr, ptep, pte);
-	/* Loading the mm will set the PTE before the operation. */
-	prev = use_temporary_mm(wr_poking_mm);
-
-	kasan_disable_current();
-	op_start(wr_poking_addr + offset_in_page(dst), psrc, len, op);
-	kasan_enable_current();
-
-	barrier();
-	/* Only after the memory operation, clear the PTE. */
-	pte_clear(wr_poking_mm, wr_poking_addr, ptep);
-
-	/*
-	 * Flushes also the "user" address spaces, even if not
-	 * existent, with PTI on. The PTE is flushed only on the
-	 * core where it exists and it is done with a "user" function,
-	 * even if the PTE is supervisor-only and global.
-	 */
-	__flush_tlb_one_user(wr_poking_addr);
-
-	/*
-	 * Loading the previous page-table hierarchy requires a
-	 * serializing instruction that already allows the core to
-	 * see the updated version. Xen-PV is assumed to serialize
-	 * execution in a similar manner.
-	 */
-	unuse_temporary_mm(prev);
-
-	pte_unmap_unlock(ptep, ptl);
-	op_end(dst, psrc, len, op);
-	local_irq_restore(flags);
-}
 
 void *__wr_op(unsigned long dst, unsigned long src, __kernel_size_t len,
 	      enum wr_op_type op)
 {
-	unsigned long size;
-	unsigned long p = dst;
+	temporary_mm_state_t prev;
+	unsigned long flags;
+	unsigned long offset;
+	unsigned long wr_poking_addr;
 
-	/* Confirm that it's past initialization. */
+	/* Confirm that the writable mapping exists. */
 	BUG_ON(!wr_ready);
 
-	if (WARN_ONCE(op >= WR_OPS_NUMBER,
-		       "Invalid Write Rare operation") ||
-	    WARN_ONCE(!is_wr_after_init(dst, len),
-		      "Write rare on invalid memory range."))
+	if (WARN_ONCE(op >= WR_OPS_NUMBER, "Invalid WR operation.") ||
+	    WARN_ONCE(!is_wr_after_init(dst, len), "Invalid WR range."))
 		return (void *)dst;
 
-	for (p = (unsigned long)dst; len; p += size, len -= size) {
-		size = min(len, PAGE_SIZE - offset_in_page(p));
-		op_page(p, &src, size, op);
-	}
+	offset = dst - (unsigned long)&__start_wr_after_init;
+	wr_poking_addr = wr_poking_base + offset;
+	local_irq_save(flags);
+	prev = use_temporary_mm(wr_poking_mm);
+
+	kasan_disable_current();
+	if (op == WR_MEMCPY)
+		memcpy((void *)wr_poking_addr, (void *)src, len);
+	else if (op == WR_MEMSET)
+		memset((u8 *)wr_poking_addr, (u8)src, len);
+	else if (op == WR_RCU_ASSIGN_PTR)
+		/* generic version of rcu_assign_pointer */
+		smp_store_release((void **)wr_poking_addr,
+				  RCU_INITIALIZER((void **)src));
+	kasan_enable_current();
+
+	barrier(); /* XXX redundant? */
+
+	unuse_temporary_mm(prev);
+	/* XXX make the verification optional? */
+	if (op == WR_MEMCPY)
+		BUG_ON(memcmp((void *)dst, (void *)src, len));
+	else if (op == WR_MEMSET)
+		BUG_ON(memtst((void *)dst, (u8)src, len));
+	else if (op == WR_RCU_ASSIGN_PTR)
+		BUG_ON(*(unsigned long *)dst != src);
+	local_irq_restore(flags);
 	return (void *)dst;
 }
 
 struct mm_struct *copy_init_mm(void);
 void __init wr_poking_init(void)
 {
-	spinlock_t *ptl;
-	pte_t *ptep;
+	unsigned long start = (unsigned long)&__start_wr_after_init;
+	unsigned long end = (unsigned long)&__end_wr_after_init;
+	unsigned long i;
+	unsigned long wr_range;
+	temporary_mm_state_t prev;
 
 	wr_poking_mm = copy_init_mm();
 	BUG_ON(!wr_poking_mm);
 
-	/* Randomize the poking address */
-	wr_poking_addr = TASK_UNMAPPED_BASE +
-		(kaslr_get_random_long("Write Rare Poking") & PAGE_MASK) %
-		(TASK_SIZE - TASK_UNMAPPED_BASE - PAGE_SIZE);
+	/* XXX What if it's too large to fit in the task unmapped mem? */
+	wr_range = round_up(end - start, PAGE_SIZE);
 
-	/*
-	 * We need to trigger the allocation of the page-tables that will be
-	 * needed for poking now. Later, poking may be performed in an atomic
-	 * section, which might cause allocation to fail.
-	 */
-	ptep = get_locked_pte(wr_poking_mm, wr_poking_addr, &ptl);
-	BUG_ON(!ptep);
-	pte_unmap_unlock(ptep, ptl);
+	/* Randomize the poking address base*/
+	wr_poking_base = TASK_UNMAPPED_BASE +
+		(kaslr_get_random_long("Write Rare Poking") & PAGE_MASK) %
+		(TASK_SIZE - (TASK_UNMAPPED_BASE + wr_range));
+
+	/* Create alternate mapping for the entire wr_after_init range. */
+	for (i = start; i < end; i += PAGE_SIZE) {
+		struct page *page;
+		spinlock_t *ptl;
+		pte_t pte;
+		pte_t *ptep;
+		unsigned long wr_poking_addr;
+
+		BUG_ON(!(page = virt_to_page(i)));
+		wr_poking_addr = i - start + wr_poking_base;
+
+		/* The lock is not needed, but avoids open-coding. */
+		ptep = get_locked_pte(wr_poking_mm, wr_poking_addr, &ptl);
+		VM_BUG_ON(!ptep);
+
+		pte = mk_pte(page, PAGE_KERNEL);
+		set_pte_at(wr_poking_mm, wr_poking_addr, ptep, pte);
+		spin_unlock(ptl);
+	}
 	wr_ready = true;
 }
